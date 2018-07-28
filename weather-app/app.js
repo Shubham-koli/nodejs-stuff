6b147369037b4725b7d91bacaf8c209c40600b60
Shubham <bkblust@gmail.com>
2018-07-28 19:21:17 +0530

    showInfo(); It shows Location and Correct address of the input Location

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,19 +1,10 @@
 const request = require('request');
 var fs = require("fs");
+const color = require('colors/safe');
 
 var baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
 var city = 'Pune';
-const APIkey = '';
-
-
-request({
-    url: baseURL + city + APIkey,
-    json: true
-}, (error, response, body) => {
-    console.log(error);
-    logLocation(body);
-    console.log(JSON.stringify(body, undefined, 2));
-});
+const APIkey = '&key=AIzaSyA4Q7APjWXCpxYG7etHAsv4V-V10yz_rZI';
 
 var logLocation = (response) =>                  
 {
@@ -22,3 +13,21 @@ var logLocation = (response) =>
         console.log('The file has been saved!');
     });
 };
+ 
+var showInfo = (body) => {
+    console.log(color.cyan(`Address is ${body.results[0].formatted_address}
+    Location:\n    Latitude: ${body.results[0].geometry.location.lat}
+    and Longitude: ${body.results[0].geometry.location.lng} `));
+}
+
+
+request({
+    url: baseURL + city + APIkey,
+    json: true
+}, (error, response, body) => {
+    if(error) console.log(error);
+    logLocation(body);
+    //console.log(JSON.stringify(body, undefined, 2));
+    showInfo(body);
+});
+
