2a736d7f75ca8fcd7115a558b2f31524a529695e
Shubham <bkblust@gmail.com>
2018-07-28 22:39:43 +0530

    now weather app can work with dynamic input

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,19 +1,33 @@
+require('dotenv').config()
+const yargs = require('yargs');
 const request = require('request');
 var fs = require("fs");
 const color = require('colors/safe');
 
-var baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-var city = 'Pune';
-const APIkey = '&key=AIzaSyA4Q7APjWXCpxYG7etHAsv4V-V10yz_rZI';
+var argv = yargs.options({
+        a: {
+            demand: true,
+            alias: 'Address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
 
-var logLocation = (response) =>                  
-{
+
+var baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+var city = encodeURIComponent(argv.a);
+const APIkey = process.env.APIkey;
+
+var logLocation = (response) => {
     fs.writeFileSync('location-data.json', JSON.stringify(response), (err) => {
         if (err) throw err;
         console.log('The file has been saved!');
     });
 };
- 
+
 var showInfo = (body) => {
     console.log(color.cyan(`Address is ${body.results[0].formatted_address}
     Location:\n    Latitude: ${body.results[0].geometry.location.lat}
@@ -25,9 +39,12 @@ request({
     url: baseURL + city + APIkey,
     json: true
 }, (error, response, body) => {
-    if(error) console.log(error);
+    if (error) console.log(error);
+    if (body.status === 'ZERO_RESULTS') {
+        console.log('No Results Found');
+        process.exit();
+    }
     logLocation(body);
     //console.log(JSON.stringify(body, undefined, 2));
     showInfo(body);
 });
-
