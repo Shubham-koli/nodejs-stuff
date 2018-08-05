7566f3ec9a5459a3d4fc5673eef8db1e0d068153
Shubham <shubham.koli@outlook.in>
2018-08-05 13:34:19 +0530

    minor fixes

--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -15,23 +15,23 @@ let geocodeAddress = (address, callback) => {
         (error, response, body) => {
             if (error) {
                 callback("Can not connect to google servers.");
-            }
-            if (body.status === "ZERO_RESULTS") {
+            } else if (body === undefined || body.status === "ZERO_RESULTS") {
                 callback("No Results Found");
-            }
-            logData(body);
-            //console.log(JSON.stringify(body, undefined, 2));
-            //showInfo(body);
-            try {
+            } else {
+                logData(body);
+                //console.log(JSON.stringify(body, undefined, 2));
+                //showInfo(body);
+                try {
 
-                callback(undefined, {
-                    address: body.results[0].formatted_address,
-                    latitude: body.results[0].geometry.location.lat,
-                    longitude: body.results[0].geometry.location.lng,
-                });
+                    callback(undefined, {
+                        address: body.results[0].formatted_address,
+                        latitude: body.results[0].geometry.location.lat,
+                        longitude: body.results[0].geometry.location.lng,
+                    });
 
-            } catch (error) {
-                console.log('Can not connect to google servers.');
+                } catch (error) {
+                    console.log('Can not connect to google servers.');
+                }
             }
         }
     );
