aca60d2e36114d6d4974c8de853fdcabcf6da927
Shubham <shubham.koli@outlook.in>
2018-08-05 13:34:39 +0530

    Trying to implement promises in main app

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,7 +5,7 @@ let fs = require("fs");
 const color = require("colors/safe");
 const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
-
+//const promise = require('./playground/promise');
 let argv = yargs
   .options({
     a: {
@@ -25,14 +25,14 @@ let location;
 //Output the weather
 geocode.geocodeAddress(address, (errorMessage, results) => {
   if (errorMessage)
-    console.log(errorMessage);
+    console.log(color.red(errorMessage));
   else {
     console.log(color.cyan(JSON.stringify(results, undefined, 2)));
     location = results;
 
     weather.getWeather(location, (errorMessage, results) => {
       if (errorMessage)
-        console.log(errorMessage);
+        console.log(color.red(errorMessage));
       else {
         console.log("Summery " + color.green(JSON.stringify(results.currently.summary)));
         console.log("Temperature " + color.green(JSON.stringify(results.currently.temperature)));
@@ -47,3 +47,5 @@ let show = (fieldname, field) => {
   if (field)
     console.log(`${fieldname} ` + color.green(JSON.stringify(field)));
 };
+//needs some work
+//promise.geocodeAddress(encodeURIComponent(argv.a));
