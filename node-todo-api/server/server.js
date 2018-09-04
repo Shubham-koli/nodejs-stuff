785cd24a99315364408a2e323d949c44e9fb56ec
Shubham <shubham.koli@outlook.in>
2018-09-04 16:15:09 +0530

    Structured the code

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,72 +1,21 @@
-var mongoose = require('mongoose');
+// library imports
+const express = require('express');
+const bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
-    useNewUrlParser: true
-}, (err) => {
-    if (err) {
-        console.log('Unable to connect MongoDB server', err);
-    }
+
+// local imports
+const {
+    mongoose
+} = require('./db/mongoose');
+const {
+    Todo
+} = require('./models/todo');
+const {
+    User
+} = require('./models/users');
+
+var app = express();
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
 });
-
-const Todo = mongoose.model('Todos', {
-    text: {
-        type: String
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
-});
-
-const User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 8,
-        trim: 1
-    },
-    userID: {
-        type: String,
-        type: String,
-        required: true,
-        minlength: 3,
-        trim: 1
-    },
-    pwd: {
-        type: String,
-        required: true,
-        minlength: 8,
-        trim: 1
-    }
-});
-// let newTodo = new Todo({
-//     text: 'Cook Dinner'
-// });
-
-// let newTodo = new Todo({
-//     text: 'Walk the dog',
-//     completed: true,
-//     completedAt: 8421999884
-// })
-
-let newUser = new User({
-    email: 'shubham.koli@outlook.in',
-    userID: 'FaultyCarry',
-    pwd: 'secret123'
-});
-
-
-newUser.save().then((doc) => {
-    console.log('User Created', doc);
-}, (err) => {
-    console.log('Unable to Create the User', err);
-});
-
-// newTodo.save().then((doc) => {
-//     console.log('Saved Todo', doc);
-// }, (err) => {
-//     console.log('Unable to save the Todo', err);
-// });
