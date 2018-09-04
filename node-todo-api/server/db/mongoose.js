64cb415680bb3c7bf19cc9e13b37b5fab160131c
Shubham <shubham.koli@outlook.in>
2018-09-04 16:14:35 +0530

    Created module for connection

---
+++ node-todo-api/server/db/mongoose.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp', {
+    useNewUrlParser: true
+}, (err) => {
+    if (err) {
+        console.log('Unable to connect MongoDB server', err);
+    }
+});
+
+module.exports = {
+    mongoose
+};
